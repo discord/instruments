c1b0b7f2ce67e30d2609c6a686f16de767b8c9e8
Daisy Zhou <daisy@discordapp.com>
2025-07-30 12:39:50 -0700

    use one ETS table per scheduler in FastGauge

--- bench/fast_gauge/gauge.exs
+++ bench/fast_gauge/gauge.exs
@@ -1,29 +1,34 @@
 Application.ensure_all_started(:instruments)
 
-Benchee.run(
-  %{
-    "gauge_non_parallel" => fn options ->
-      for v <- 1..100 do
-        Instruments.Statix.gauge("test.gauge", v, options)
-      end
-    end,
-    "fastgauge_non_parallel" => fn options ->
-      for v <- 1..100 do
-        Instruments.FastGauge.gauge("test.gauge", v, options)
-      end
-    end
-  },
-  inputs: %{
-    "1. No Options" => [],
-    "2. No Tags" => [sample_rate: 1.0],
-    "3. Empty Tags" => [sample_rate: 1.0, tags: []],
-    "4. One Tag" => [sample_rate: 1.0, tags: ["test:tag"]],
-    "5. Five Tags" => [sample_rate: 1.0, tags: ["test-1:tag", "test-2:tag", "test-3:tag", "test-4:tag", "test-5:tag"]],
-    "6. Ten Tags" => [sample_rate: 1.0, tags: ["test-1:tag", "test-2:tag", "test-3:tag", "test-4:tag", "test-5:tag", "test-6:tag", "test-7:tag", "test-8:tag", "test-9:tag", "test-10:tag"]]
-  },
-  parallel: 1,
-  save: [path: "bench/results/fast_gauge/non_parallel.benchee"]
-)
+# Benchee.run(
+#   %{
+#     "gauge_non_parallel" => fn options ->
+#       for v <- 1..100 do
+#         Instruments.Statix.gauge("test.gauge", v, options)
+#       end
+#     end,
+#     "fastgauge_non_parallel" => fn options ->
+#       for v <- 1..100 do
+#         Instruments.FastGauge.gauge("test.gauge", v, options)
+#       end
+#     end,
+#     "fastgauge_multitable_non_parallel" => fn options ->
+#       for v <- 1..100 do
+#         Instruments.FasterGauge.gauge("test.gauge", v, options)
+#       end
+#     end
+#   },
+#   inputs: %{
+#     "1. No Options" => [],
+#     "2. No Tags" => [sample_rate: 1.0],
+#     "3. Empty Tags" => [sample_rate: 1.0, tags: []],
+#     "4. One Tag" => [sample_rate: 1.0, tags: ["test:tag"]],
+#     "5. Five Tags" => [sample_rate: 1.0, tags: ["test-1:tag", "test-2:tag", "test-3:tag", "test-4:tag", "test-5:tag"]],
+#     "6. Ten Tags" => [sample_rate: 1.0, tags: ["test-1:tag", "test-2:tag", "test-3:tag", "test-4:tag", "test-5:tag", "test-6:tag", "test-7:tag", "test-8:tag", "test-9:tag", "test-10:tag"]]
+#   },
+#   parallel: 1,
+#   save: [path: "bench/results/fast_gauge/non_parallel.benchee"]
+# )
 
 Benchee.run(
   %{
@@ -35,6 +40,11 @@ Benchee.run(
     "fastgauge_parallel_8" => fn options ->
       for v <- 1..100 do
         Instruments.FastGauge.gauge("test.gauge", v, options)
+      end
+    end,
+    "fastgauge_multitable_parallel_8" => fn options ->
+      for v <- 1..100 do
+        Instruments.FasterGauge.gauge("test.gauge", v, options)
       end
     end
   },
--- lib/fast_gauge.ex
+++ lib/fast_gauge.ex
@@ -1,5 +1,15 @@
 defmodule Instruments.FastGauge do
-  @table_name :instruments_gauges
+  @moduledoc false
+  # Instruments.FastGauge sets up one ETS table per scheduler, and calls to
+  # `gauge/3` insert into the table for the current scheduler.
+  # The key for the table entry is built out of the gauge name and options.
+  # The value for the table entry includes the gauge value as well as the timestamp
+  # it was recorded at.
+  #
+  # The Instruments.FastGauge process periodically reports the most recent value
+  # for every table key, deleting values that have been reported or ignored.
+  @table_prefix :instruments_gauges
+  @max_tables 128
   @report_interval_ms Application.compile_env(
                         :instruments,
                         :fast_gauge_report_interval,
@@ -19,12 +29,16 @@ defmodule Instruments.FastGauge do
   end
 
   def init(:ok) do
-    :ets.new(@table_name, [:named_table, :public, :set])
+    table_count = :erlang.system_info(:schedulers)
+
+    for scheduler_id <- 1..table_count do
+      :ets.new(table_name(scheduler_id), [:named_table, :public, :set])
+    end
 
     reporter_module = Application.get_env(:instruments, :reporter_module, Instruments.Statix)
 
     schedule_report()
-    {:ok, reporter_module}
+    {:ok, {reporter_module, table_count}}
   end
 
   ## Public
@@ -33,21 +47,37 @@ defmodule Instruments.FastGauge do
   @spec gauge(iodata, integer, Statix.options()) :: :ok
   def gauge(name, value, options \\ []) do
     table_key = get_table_key(name, options)
-    :ets.insert(@table_name, [{table_key, value}])
+    timestamp = System.monotonic_time()
+    :ets.insert(current_table(), [{table_key, {value, timestamp}}])
   end
 
   ## GenServer callbacks
-  def handle_info(:report, reporter_module = state) do
-    @table_name
-    |> :ets.tab2list()
-    |> Enum.each(
-      fn {table_key, value} ->
-        report_stat({table_key, value}, reporter_module)
+  def handle_info(:report, {reporter_module, table_count} = state) do
+    1..table_count
+    |> Enum.map(fn scheduler_id -> table_name(scheduler_id) end)
+    |> Enum.reduce(%{}, fn table_name, acc ->
+      # Aggregate the "most recent" value for each key across all tables
+      table_name
+      |> :ets.tab2list()
+      |> Enum.reduce(
+        acc,
+        fn {table_key, {value, timestamp}}, acc ->
+          :ets.delete_object(table_name, {table_key, {value, timestamp}})
 
-        # Delete the object only if the current value is the one we just reported
-        # If we reported A and the value went from A -> B -> A, and we never report B,
-        # oh well, that's just how a gauge works.
-        :ets.delete_object(@table_name, {table_key, value})
+          # Only keep most recent timestamp
+          Map.update(acc, table_key, {value, timestamp}, fn {existing_value, existing_timestamp} ->
+            if existing_timestamp > timestamp do
+              {existing_value, existing_timestamp}
+            else
+              {value, timestamp}
+            end
+          end)
+        end
+      )
+    end)
+    |> Enum.each(
+      fn {table_key, {value, _timestamp}} ->
+        report_stat({table_key, value}, reporter_module)
       end
     )
 
@@ -84,5 +114,15 @@ defmodule Instruments.FastGauge do
   defp schedule_report() do
     wait_time = @report_interval_ms + Enum.random(@report_jitter_range_ms)
     Process.send_after(self(), :report, wait_time)
+  end
+
+  defp current_table() do
+    table_name(:erlang.system_info(:scheduler_id))
+  end
+
+  for scheduler_id <- 1..@max_tables do
+    defp table_name(unquote(scheduler_id)) do
+      unquote(:"#{@table_prefix}_#{scheduler_id}")
+    end
   end
 end
